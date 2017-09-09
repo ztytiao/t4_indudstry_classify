53a597f28bb5c9c49e451c7bebe08b4f98fa08f4
ztytiao <ztytiao@126.com>
2017-09-10 00:31:23 +0800

    optimize classifier
    优化了分类结构

--- bin/nlpClassify.py
+++ bin/nlpClassify.py
@@ -1,5 +1,6 @@
 import jieba
 import pandas as pd
+import random
 import numpy as np
 from sklearn import feature_extraction
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -23,6 +24,8 @@ def readFilesName(dir):
 
 def readExcel(url):
     df=pd.read_excel(url,na_values='',index_col=None)
+    df['ABSTRACT']=df['ABSTRACT'].astype(str)
+    #df['ABSTRACT']=df['ABSTRACT']+'Nan'
     return df
 
 def combine_Abstract(df,df_Base,df_Test):
@@ -30,23 +33,36 @@ def combine_Abstract(df,df_Base,df_Test):
     df=df.where(df.notnull(),1)
     dft=df[df['Cat']==1]
     df=df[df['Cat']!=1]
-    df_Test=pd.concat([df_Test,dft],axis=0,ignore_index=True)
+
     df_Base=pd.concat([df_Base,df],axis=0,ignore_index=True)
+    df_Test=pd.concat([df_Test,dft],axis=0,ignore_index=True)
     return df_Base, df_Test
-    # for i in df.index:
-    #     if df.loc[i,'Cat'] in df_Base['Cat'].values:
-    #         index_Cat=df_Base[df_Base['Cat']==df.loc[i,'Cat']].index[0]
-    #         df_Base.loc[index_Cat,'ABSTRACT']=df_Base.loc[index_Cat,'Cat']+df.loc[i,'ABSTRACT']
-    #     else:
-    #         item_new={'Cat':df.loc[i,'Cat'],'ABSTRACT':df.loc[i,'ABSTRACT']}
-    #         df_Base=df_Base.append(item_new,ignore_index=True)
-    # return
+def combine_Abstract_B(df,df_Base,df_Test):
+    '''combine abstract with same cat'''
+    df=df.where(df.notnull(),1)
+    dft=df[df['Cat']==1]
+    df=df[df['Cat']!=1]
+    df_Temp=pd.DataFrame()
+    for i in df.index:
+        cat=df.loc[i,'Cat'].split(' ')
+        for j in cat:
+            df_Temp=df_Temp.append({'ID':df.loc[i,'ID'],
+                                    'TITLE':df.loc[i,'TITLE'],
+                                    'ABSTRACT':df.loc[i,'ABSTRACT'],
+                                    'APPLICATION':df.loc[i,'APPLICATION'],
+                                    'IPC':df.loc[i,'IPC'],
+                                    'Cat': j
+                                    },ignore_index=True)
+        pass
+    df_Base=pd.concat([df_Base,df_Temp],axis=0,ignore_index=True)
+    df_Test=pd.concat([df_Test,dft],axis=0,ignore_index=True)
+    return df_Base, df_Test
 
 def digitize_Cat(df_Base):
     Cat=pd.DataFrame(df_Base['Cat'].unique().tolist(),columns=['Cat'])
     Cat['id']=Cat.index
     df_Base2=pd.merge(df_Base,Cat,'left',on='Cat')
-    return df_Base2
+    return df_Base2,Cat
     pass
 
 def __TFgetjiebaSeries(ds):
@@ -58,6 +74,7 @@ def __TFgetjiebaSeries(ds):
             subcor.append(c)
         joint = ' '
         subcor = joint.join(subcor)
+
         cor.append(subcor)
     return cor
 
@@ -102,8 +119,25 @@ def TFtrainandtest_Real(xtrain,xtest,ytrain,clf=GaussianNB(),test_size=0.2,ada=0
 
     ypredict=clff.predict(weighttest.tolist())
 
-
     return ypredict
+
+def TFtrain(xtrain,ytrain,clf=GaussianNB(),test_size=0.2,ada=0,nestimators=100):
+    '''x，y,clf,test_size,ada=0为不使用ada，IPCmat默认不使用'''
+    #xtrain,xtest,ytrain
+    xtraincor=__TFgetjiebaSeries(xtrain)
+    #xtestcor=__TFgetjiebaSeries(xtest)
+
+    #获取分词矩阵
+    word, weighttrain, vector, transformer = TFtraintfidf(xtraincor)
+    #weighttest = TFtesttfidf(xtestcor, vector, transformer)
+
+    #ada boost
+    if ada>0:
+        clff=adaboost(clf,weighttrain.tolist(),ytrain.tolist(),learning_rate=1,n_estimators=nestimators)
+    else:
+        clff= TFtraining(weighttrain.tolist(), ytrain.tolist())
+    #ypredict = clff.predict(weighttest.tolist())
+    return clff,vector,transformer
 
 def TFtesttfidf(cor,vectorizer,transformer):
     '''根据已经获取的，计算测试集的权重'''
@@ -113,10 +147,11 @@ def TFtesttfidf(cor,vectorizer,transformer):
     return weight
 def TFtraintfidf(cor):
     '''获取词语集合，以及权重'''
-    vectorizer = CountVectorizer()  # 该类会将文本中的词语转换为词频矩阵，矩阵元素a[i][j] 表示j词在i类文本下的词频  
+    vectorizer = CountVectorizer(max_features=3000)  # TODO 该类会将文本中的词语转换为词频矩阵，矩阵元素a[i][j] 表示j词在i类文本下的词频  
     transformer = TfidfTransformer()  # 该类会统计每个词语的tf-idf权值  
     train = transformer.fit_transform(vectorizer.fit_transform(cor))  # 第一个fit_transform是计算tf-idf，第二个fit_transform是将文本转为词频矩阵  
     word = vectorizer.get_feature_names()  # 获取词袋模型中的所有词语  
+    print('-----------------size of feature is',len(word))
     weight = train.toarray()  # 将tf-idf矩阵抽取出来，元素a[i][j]表示j词在i类文本中的tf-idf权重  
     return word,weight,vectorizer,transformer
 def adaboost(clf,X_train,y_train,learning_rate=1,n_estimators=100):
@@ -131,25 +166,82 @@ def TFtesting(x,y,clf):
     ypredict=clf.predict(x)
     return y,ypredict
 
+def SplitTrainning(df_Base,size_Train,df_Predict,size_Predict,Cat,repeatNo):
+    '''预测'''
+    del df_Predict['Cat']
+    df=pd.DataFrame(index=df_Predict['ID'],columns=['predict'])
+    df['predict']=' '
+    batch_Predict=int(df_Predict.shape[0]/size_Predict)#多批次预测
+    for j in range(repeatNo):
+        '''构建多个小分类器'''
+        print('Now Traning the ',j,' th classifly')
+        tic=time.clock()
+        toc=time.clock()
+        Y=[]
+        indSeleted = random.sample(df_Base.index.tolist(), size_Train)
+        df_Train = df_Base.loc[indSeleted]
+        clff, vector, transformer=TFtrain(xtrain=df_Train['ABSTRACT'],  ytrain=df_Train['id'],
+                                       clf=GaussianNB(), test_size=0.7, ada=1, nestimators=50)
+        for i in range(batch_Predict):
+            indSeleted2=range(i*size_Predict,(i+1)*size_Predict)
+            df_T=df_Predict.loc[indSeleted2]
+            xtestcor = __TFgetjiebaSeries(df_T['ABSTRACT'])
+            weighttest = TFtesttfidf(xtestcor, vector, transformer)
+            ypredict =clff.predict(weighttest.tolist())
+            Y=Y+ypredict.tolist()
+            print('Predict',i*size_Predict,' th record-------------------',time.clock())
+        indSeleted2 = range(batch_Predict * size_Predict, df_Predict.shape[0])
+        df_T = df_Predict.loc[indSeleted2]
+        ypredict = TFtrainandtest_Real(xtrain=df_Train['ABSTRACT'], xtest=df_T['ABSTRACT'], ytrain=df_Train['id'],
+                                       clf=GaussianNB(), test_size=0.7, ada=1, nestimators=50)
+        Y = Y + ypredict.tolist()
+        Y=[str(i) for i in Y]
+        df['predict']=df['predict']+' '+Y
+        print('Total time',time.clock(),'This round takes',time.clock()-toc)
+        toc=time.clock()
+    return df
+    pass
+
+def writeExcel(df,url='',row_Max=500000):
+    No=int(df.shape[0]/row_Max)
+    for i in range(No):
+        u=url+'file'+str(i)+'.xlsx'
+        write = pd.ExcelWriter(u)
+        df[i*row_Max:(i+1)*row_Max].to_excel(write, sheet_name='Sheet1',merge_cells=False)
+        write.save()
+
+    u = url + 'file' + str(No) + '.xlsx'
+    write = pd.ExcelWriter(u)
+    df[No * row_Max:df.shape[0]].to_excel(write, sheet_name='Sheet1', merge_cells=False)
+    write.save()
 
 if __name__=="__main__":
     #1.inial
     docdir = os.path.abspath('..') + '\\doc\\'
     name=readFilesName(docdir+'mid\\')
-    df_Base = pd.DataFrame(None, columns=['Cat', 'ABSTRACT'])
-    df_Test = pd.DataFrame(None, columns=['Cat', 'ABSTRACT'])
+    dest=docdir+'re2\\'
+    df_Base = pd.DataFrame()
+    df_Test = pd.DataFrame()
+
     #2.get the ready Data
+    print('-------------Start Reading Data--------------')
+    j=0
     for i in name:
         df=readExcel(i)
         df_Base,df_Test= combine_Abstract(df,df_Base,df_Test)
+        j=j+1
+        print(j/len(name),'Data Loaded')
 
     #3.train the data
-    df_Base=digitize_Cat(df_Base)
-    ypredict = TFtrainandtest_Real(xtrain=df_Base['ABSTRACT'],xtest=df_Test['ABSTRACT'], ytrain= df_Base['id'],
-                                       clf=GaussianNB(), test_size=0.7, ada=1, nestimators=100)
+    print('-------------Start Training Data--------------')
+    df_Base,Cat=digitize_Cat(df_Base)
 
+    df=SplitTrainning(df_Base,200,df_Test,500,Cat,1)
+    # 上面有三个数字，第一个代表训练集大小，第二个代表预测集大小，第三个表示有多少个弱分类器
     #4.export result
-    
-
-
+    print(df)
+    writeExcel(df, url=dest, row_Max=500000)#结果输出，其中数字代表一个文件里面有多少条记录，会拆分
+    write = pd.ExcelWriter(dest+'Cat.xlsx')
+    Cat.to_excel(write, sheet_name='Sheet1', merge_cells=False)
+    write.save()
     pass
